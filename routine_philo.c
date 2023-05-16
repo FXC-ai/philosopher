d70950373ecf47d060a525781f8e760cc0558570
Francois-xavier Coindreau <fcoindre@c1r5s7.42lausanne.ch>
2023-05-16 13:49:49 +0200

    issue with stopping when nb_meals reached almost fixed

--- routine_philo.c
+++ routine_philo.c
@@ -72,6 +72,14 @@ void	eat(t_philo *philo)
 {
     if (check_death(philo) == 0 && check_stop(philo) == 0)
     {
+		
+	    pthread_mutex_lock(philo->chopstick_right);
+	    printf("%ld ms : %d has taken a fork (right)\n", calculate_current_time_ms(philo->start_time), philo->id);
+
+        pthread_mutex_lock(philo->chopstick_left);
+        printf("%ld ms : %d has taken a fork (left)\n",calculate_current_time_ms(philo->start_time), philo->id);
+
+
         printf("\033[1;3%dm%ld ms : %d is eating\n\033[0m", (philo->id % 7),calculate_current_time_ms(philo->start_time), philo->id);
         ft_usleep(philo->rules->time_to_eat);
         philo->time_last_eat = calculate_current_time_ms(philo->start_time);
@@ -92,7 +100,8 @@ void	eat(t_philo *philo)
 		philo->priority = 0;
 		pthread_mutex_unlock(philo->mut_protect_priority);
 
-
+        pthread_mutex_unlock(philo->chopstick_right);
+		pthread_mutex_unlock(philo->chopstick_left);
         //printf("\033[1;3%dm%ld ms : Philosopher %d finish to eat.\n\033[0m", (philo->id % 7), philo->time_last_eat, philo->id);
     }
 }
@@ -149,11 +158,11 @@ void *routine_philosopher(void *philo)
 		if (read_priority(cpy_philo) == 1 && check_nb_meals(cpy_philo) == 0)
 		{
 			//printf("poire\n");
-			take_right_fork (cpy_philo);
-			take_left_fork(cpy_philo);
+			//take_right_fork (cpy_philo);
+			//take_left_fork(cpy_philo);
 			eat(cpy_philo); 
-			put_right_fork(cpy_philo);	
-			put_left_fork(cpy_philo);	
+			//put_right_fork(cpy_philo);	
+			//put_left_fork(cpy_philo);	
 			have_a_nape(cpy_philo);
 			think(cpy_philo);
 
