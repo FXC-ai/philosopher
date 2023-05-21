85ee7c4530c17264ea74b6bae726954cfbbf5663
Francois-xavier Coindreau <fcoindre@c1r5s7.42lausanne.ch>
2023-05-21 19:18:39 +0200

    Issue with 2 310 200 100

--- philo/philo.h
+++ philo/philo.h
@@ -1,11 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fcoindre <fcoindre@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/21 18:33:23 by fcoindre          #+#    #+#             */
+/*   Updated: 2023/05/21 18:38:39 by fcoindre         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PHILO_H
 # define PHILO_H
 
-#include <pthread.h>
-#include <sys/time.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+# include <pthread.h>
+# include <sys/time.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 typedef struct s_rules
 {
@@ -17,14 +29,13 @@ typedef struct s_rules
 	int				end;
 
 	pthread_mutex_t	*mut_end;
-} t_rules;
+}	t_rules;
 
-typedef struct s_philo 
+typedef struct s_philo
 {
-
 	pthread_t		tid;
 
-	pthread_mutex_t *mut_end;
+	pthread_mutex_t	*mut_end;
 	pthread_mutex_t	*chopstick_right;
 	pthread_mutex_t	*chopstick_left;
 
@@ -39,50 +50,48 @@ typedef struct s_philo
 
 	t_rules			*rules;
 
-} t_philo;
-
-
+}	t_philo;
 
 /* philo_creators.c */
 pthread_mutex_t	**create_tab_mutex(int n);
-t_rules	*init_rules(int number_of_philo, time_t *tab_times, int number_of_meal);
-t_philo	**create_tab_philosophers(pthread_mutex_t **t_chopstick, t_rules *rules);
+t_rules			*init_rules(int nb_of_ph, time_t *tab_times, int nb_of_meal);
+t_philo			**create_tab_philosophers(pthread_mutex_t **t_ck, t_rules *rls);
 
 /* free_philos.c */
-void	free_tab_mutex(pthread_mutex_t **tab_mutex);
-void	free_tab_philo(t_philo **tab_philo);
-void	free_rules(t_rules *rules);
+void			free_tab_mutex(pthread_mutex_t **tab_mutex);
+void			free_tab_philo(t_philo **tab_philo);
+void			free_rules(t_rules *rules);
 
 /* args_parser.c */
-int		ft_isdigit(int c);
-int		ft_isnumeric (char *str);
-int		check_four_args(char **argv);
-int		check_fifth_arg(char **argv);
-int		parse_args(int argc, char *argv[]);
+int				ft_isdigit(int c);
+int				ft_isnumeric(char *str);
+int				check_four_args(char **argv);
+int				check_fifth_arg(char **argv);
+int				parse_args(int argc, char *argv[]);
 
+/* utils.c */
+time_t			calculate_current_time_ms(time_t start_time_ms);
+time_t			ft_time(void);
+void			ft_usleep(time_t time_in_ms, t_philo *philo);
 
-/*utils.c*/
+/* utils_routine.c */
+int				read_end(t_philo *philo);
+int				check_death(t_philo *philo);
+int				check_nb_meals(t_philo *philo);
+void			take_right_chopstick(t_philo *philo);
+void			take_left_chopstick(t_philo *philo);
 
-
-time_t	calculate_current_time_ms (time_t start_time_ms);
-time_t	ft_time(void);
-void	ft_usleep(time_t time_in_ms, t_philo *philo);
-
-/*routine_philo.c*/
-void	take_right_fork(t_philo *philo);
-void	take_left_fork(t_philo *philo);
-void	take_a_fork(t_philo *philo);
-void	eat(t_philo *philo);
-void	put_right_fork(t_philo *philo);
-void	put_left_fork(t_philo *philo);
-void	have_a_nape(t_philo *philo);
-void    *routine_philosopher(void *philo);
-int		read_priority(t_philo	*philo);
-int		check_death(t_philo *philo);
-
-void	*routine_manager(void *tab_manager);
-
-long	ft_atoi(const char *str);
-
-
+/* routine_philo.c */
+void			take_right_fork(t_philo *philo);
+void			take_left_fork(t_philo *philo);
+void			take_a_fork(t_philo *philo);
+void			eat(t_philo *philo);
+void			put_right_fork(t_philo *philo);
+void			put_left_fork(t_philo *philo);
+void			have_a_nape(t_philo *philo);
+void			*routine_philosopher(void *philo);
+int				read_priority(t_philo *philo);
+int				check_death(t_philo *philo);
+void			*routine_manager(void *tab_manager);
+long			ft_atoi(const char *str);
 #endif
--- philo/routine_philo.c
+++ philo/routine_philo.c
@@ -6,32 +6,34 @@
 /*   By: fcoindre <fcoindre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/21 13:17:31 by fcoindre          #+#    #+#             */
-/*   Updated: 2023/05/21 13:53:21 by fcoindre         ###   ########.fr       */
+/*   Updated: 2023/05/21 19:18:03 by fcoindre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	read_end (t_philo *philo)
+int	read_end(t_philo *philo)
 {
 	int	result;
 
 	pthread_mutex_lock(philo->rules->mut_end);
 	result = philo->rules->end;
-	pthread_mutex_unlock(philo->rules->mut_end );
+	pthread_mutex_unlock(philo->rules->mut_end);
 	return (result);
 }
 
 int	check_death(t_philo *philo)
 {
-	if ((calculate_current_time_ms(philo->start_time) - philo->time_last_eat) > philo->rules->time_to_die)
+	if ((calculate_current_time_ms(philo->start_time) - philo->time_last_eat)
+		> philo->rules->time_to_die)
 	{
 		if (philo->is_dead == 0 && read_end(philo) == 0)
 		{
 			philo->is_dead = 1;
-			printf("\033[1;3%dm%ld %d died\n\033[0m", philo->id % 7, calculate_current_time_ms(philo->start_time), philo->id + 1);
+			printf("\033[1;31m%ld %d died\n\033[0m",
+				calculate_current_time_ms(philo->start_time), philo->id + 1);
 		}
-		pthread_mutex_lock(philo->rules->mut_end);		
+		pthread_mutex_lock(philo->rules->mut_end);
 		if (philo->rules->end == 0)
 		{
 			philo->rules->end = 1;
@@ -53,35 +55,62 @@ int	check_nb_meals(t_philo *philo)
 	return (0);
 }
 
+void	take_right_chopstick(t_philo *philo)
+{
+	if (check_death(philo) == 0
+		&& read_end(philo) == 0
+		&& philo->chopstick_taken == 0)
+	{
+		/* C'est ici le pb il faut verifier que les fourchettes sont dispos avant d essayer de les prendre...*/
+
+
+		pthread_mutex_lock(philo->chopstick_right);
+		philo->chopstick_taken += 1;
+		if (read_end(philo) == 0)
+		{
+			printf("%ld %d has taken a fork\n",
+				calculate_current_time_ms(philo->start_time),
+				philo->id + 1);
+		}
+	}
+}
+
+void	take_left_chopstick(t_philo *philo)
+{
+	if (check_death(philo) == 0
+		&& read_end(philo) == 0
+		&& philo->rules->number_of_philo > 1)
+	{
+		pthread_mutex_lock(philo->chopstick_left);
+		philo->chopstick_taken += 1;
+		if (read_end(philo) == 0)
+		{
+			printf("%ld %d has taken a fork\n",
+				calculate_current_time_ms(philo->start_time), philo->id + 1);
+		}
+	}
+}
+
 void	eat(t_philo *philo)
 {
 	if (check_death(philo) == 0 && read_end(philo) == 0)
 	{
-		if (check_death(philo) == 0 && read_end(philo) == 0 && philo->chopstick_taken == 0)
+		take_right_chopstick(philo);
+		take_left_chopstick(philo);
+		if (check_death(philo) == 0
+			&& read_end(philo) == 0
+			&& philo->chopstick_taken == 2)
 		{
-			pthread_mutex_lock(philo->chopstick_right);
-			philo->chopstick_taken += 1;
-			if (read_end(philo) == 0)
-				printf("%ld %d has taken a fork\n", calculate_current_time_ms(philo->start_time), philo->id + 1);
-		}
-		if (check_death(philo) == 0 && read_end(philo) == 0 && philo->rules->number_of_philo > 1)
-		{
-			pthread_mutex_lock(philo->chopstick_left);
-			philo->chopstick_taken += 1;
-			if (read_end(philo) == 0)
-				printf("%ld %d has taken a fork\n",calculate_current_time_ms(philo->start_time), philo->id + 1);
-		}
-		if (check_death(philo) == 0 && read_end(philo) == 0 && philo->chopstick_taken == 2)
-		{
-			printf("\033[1;3%dm%ld %d is eating\n\033[0m", (philo->id % 7), calculate_current_time_ms(philo->start_time), philo->id + 1);
+			printf("%ld %d is eating\n",
+				calculate_current_time_ms(philo->start_time), philo->id + 1);
 			philo->time_last_eat = calculate_current_time_ms(philo->start_time);
 			ft_usleep(philo->rules->time_to_eat, philo);
 		}
-		if (check_death(philo) == 0 && read_end(philo) == 0 && philo->rules->number_of_philo > 1)
-		{
+		if (check_death(philo) == 0
+			&& read_end(philo) == 0 && philo->rules->number_of_philo > 1)
 			philo->nb_of_eat += 1;
-		}
-		if (check_death(philo) == 0 && read_end(philo) == 0 && philo->rules->number_of_philo > 1)
+		if (check_death(philo) == 0
+			&& read_end(philo) == 0 && philo->rules->number_of_philo > 1)
 		{
 			philo->chopstick_taken = 0;
 			pthread_mutex_unlock(philo->chopstick_right);
@@ -92,28 +121,36 @@ void	eat(t_philo *philo)
 
 void	have_a_nape(t_philo *philo)
 {
-    if (check_death(philo) == 0 && read_end(philo) == 0 && check_nb_meals(philo) == 0 && philo->rules->number_of_philo > 1)
-    {
-		printf("%ld %d is sleeping\n", calculate_current_time_ms(philo->start_time),philo->id + 1);
+	if (check_death(philo) == 0
+		&& read_end(philo) == 0
+		&& check_nb_meals(philo) == 0
+		&& philo->rules->number_of_philo > 1)
+	{
+		printf("%ld %d is sleeping\n",
+			calculate_current_time_ms(philo->start_time), philo->id + 1);
 		ft_usleep(philo->rules->time_to_sleep, philo);
 	}
 }
 
 void	think(t_philo *philo)
 {
-	if (check_death(philo) == 0 && read_end(philo) == 0 && check_nb_meals(philo) == 0 && philo->rules->number_of_philo > 1)
+	if (check_death(philo) == 0
+		&& read_end(philo) == 0
+		&& check_nb_meals(philo) == 0 && philo->rules->number_of_philo > 1)
 	{
 		if (read_end(philo) == 0)
-			printf("%ld %d is thinking\n", calculate_current_time_ms(philo->start_time),philo->id + 1);
+		{
+			printf("%ld %d is thinking\n",
+				calculate_current_time_ms(philo->start_time), philo->id + 1);
+		}
 	}
 }
 
-void *routine_philosopher(void *philo)
+void	*routine_philosopher(void *philo)
 {
 	t_philo			*cpy_philo;
 
 	cpy_philo = (t_philo *) philo;
-
 	if (cpy_philo->id % 2 == 1)
 	{
 		ft_usleep((cpy_philo->rules->time_to_eat / 2), philo);
@@ -122,19 +159,18 @@ void *routine_philosopher(void *philo)
 	{
 		if (check_nb_meals(cpy_philo) == 0)
 		{
-			eat(cpy_philo); 
+			eat(cpy_philo);
 			have_a_nape(cpy_philo);
 			think(cpy_philo);
 		}
-		if (check_nb_meals(cpy_philo) == 1)
+		if (check_nb_meals (cpy_philo) == 1)
 		{
-			break;
+			break ;
 		}
-		if (check_death(cpy_philo) == 1)
+		if (check_death (cpy_philo) == 1)
 		{
-			break;
+			break ;
 		}
-
 	}
 	return (NULL);
 }
